79f978d5871ef8800ac27ff27639942ed46757b5
Kintaro Minegishi <minegishikintaro@wlan-napt-001.u-aizu.ac.jp>
2018-07-05 19:27:16 +0900

    Won / lost message added

--- dicegame.c
+++ dicegame.c
@@ -4,6 +4,14 @@
 
 int main(void){
   int s1,s2,t;
+  char name[20];
+
+  printf("What is your name?\n");
+  printf(">");
+  scanf("%s",name);
+
+  printf("Hello, %s!! \n",name);
+
 
   srand(time(NULL));
 
@@ -14,6 +22,12 @@ int main(void){
   printf("Die 1 : %d\n",s1);
   printf("Die 2 : %d\n",s2);
   printf("Total value : %d\n",t);
+
+  if(t>=7){
+    printf("You won!\n");
+  }
+  else {printf("You lost!\n");}
+
 
 
   return 0;
